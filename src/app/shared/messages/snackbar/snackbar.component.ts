a645538844228adede55d2615b4885c0e4fd32ab
dronan <dronan@gmail.com>
2019-06-02 19:26:03 -0300

    trocando o subscribe para do para que o observable fique dependente um do outro

--- src/app/shared/messages/snackbar/snackbar.component.ts
+++ src/app/shared/messages/snackbar/snackbar.component.ts
@@ -1,10 +1,12 @@
-import {Observable} from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate} from "@angular/animations"
 
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import {NotificationService} from '../notification.service'
+import {Observable} from 'rxjs/Observable'
+import 'rxjs/add/observable/timer'
+import 'rxjs/add/operator/do'
+import 'rxjs/add/operator/switchMap'
 
-import { NotificationService } from '../notification.service'
-import 'rxjs/add/observable/timer';
 
 @Component({
   selector: 'mt-snackbar',
@@ -33,13 +35,13 @@ export class SnackbarComponent implements OnInit {
   snackVisibility: string = 'hidden';
 
   ngOnInit() {
-    this.notificationService.notifier.subscribe(message => {
-      this.message = message;
-      this.snackVisibility = 'visible';
-      Observable.timer(3000).subscribe(timer => {
-        this.snackVisibility = 'hidden'
-      })
+    this.notificationService.notifier
+    .do(message => {
+      this.message = message
+      this.snackVisibility = 'visible'
     })
+    .switchMap(message => Observable.timer(3000) )
+    .subscribe(timer => this.snackVisibility = 'hidden' )
   }
 
 }
