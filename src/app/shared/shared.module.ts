51fc7141d8bb66792307ba2507ee74a18ca808fa
dronan <dronan@gmail.com>
2019-06-02 15:58:46 -0300

    criando animacao no rodape

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,12 +8,13 @@ import {InputComponent} from './input/input.component';
 import { ShoppingCartService } from 'app/restaurant-detail/shopping-cart/shopping-cart.sevice';
 import { RestaurantsService } from 'app/restaurants/restaurants.service';
 import { OrderService } from 'app/order/order.service';
+import { SnackbarComponent } from './messages/snackbar/snackbar.component';
 
 
 @NgModule({
-    declarations:[RadioComponent, InputComponent, RatingComponent],
+    declarations:[RadioComponent, InputComponent, RatingComponent, SnackbarComponent],
     imports: [CommonModule, ReactiveFormsModule, FormsModule],
-    exports: [RadioComponent, InputComponent, RatingComponent, CommonModule, ReactiveFormsModule, FormsModule]
+    exports: [RadioComponent, InputComponent, RatingComponent, CommonModule, ReactiveFormsModule, FormsModule, SnackbarComponent]
 })
 
 export class SharedModule {
