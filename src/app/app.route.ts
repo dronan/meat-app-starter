0e28594085a0ccbc4970f79e491cbadbcb0ffd34
dronan <dronan@gmail.com>
2019-06-02 14:04:43 -0300

    criando lazyloading

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -1,12 +1,12 @@
 import {Routes} from '@angular/router'
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { OrderComponent } from './order/order.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
+//import { AboutModule } from './about/about.module';
 
 
 export const ROUTES : Routes = [
@@ -18,7 +18,7 @@ export const ROUTES : Routes = [
             {path: 'menu',  component: MenuComponent},
             {path: 'reviews',  component: ReviewsComponent}
         ]},
-    {path: 'about', component: AboutComponent},
+    {path: 'about', loadChildren: './about/about.module#AboutModule'},
     {path: 'order', component: OrderComponent},
     {path: 'order-summary', component: OrderSummaryComponent}
 ];
