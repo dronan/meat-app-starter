5f4d1ad649076fb63d69a63dea4513b3fa69b081
diegokulian <diego.kulian@lbca.com.br>
2020-09-25 21:31:41 -0300

    refinamento

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -5,7 +5,6 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 import { FormBuilder, FormControl, FormGroup }  from '@angular/forms'
 
 import 'rxjs/add/operator/switchMap'
-import 'rxjs/add/operator/do'
 import 'rxjs/add/operator/debounceTime'
 import 'rxjs/add/operator/distinctUntilChanged'
 
@@ -51,11 +50,8 @@ export class RestaurantsComponent implements OnInit {
     this.searchControl.valueChanges
       .debounceTime(500)
       .distinctUntilChanged()
-      .do( searchTerm => console.log(`q=${searchTerm}`))
-      .switchMap( 
-      searchTerm => 
-        this.restaurantsService.restaurants(searchTerm))
-      .subscribe(restaurants => this.restaurants = restaurants);
+      .switchMap( searchTerm => this.restaurantsService.restaurants(searchTerm))
+      .subscribe( restaurants => this.restaurants = restaurants);
 
   }
 
